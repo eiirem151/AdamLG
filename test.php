8eafebb46b8a9d6b834d57aa99f1741bb2584697
eiirem151 <164561172+eiirem151@users.noreply.github.com>
2024-03-23 17:28:16 +0100

    Delete test.php

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-$content = file_get_contents('https://breezy-home-609608.framer.app/');
-$content = str_replace('</title>','</title><base href="https://www.google.com/calendar/" />', $content);
-$content = str_replace('</head>','<link rel="stylesheet" href="/test.css" /></head>', $content);
-echo $content;
